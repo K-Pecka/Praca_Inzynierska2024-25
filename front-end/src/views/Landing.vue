c252c86318f2ca99b6cf28d1dbd79e2775d99c17
KacperPecka <s25668@pjwstk.edu.pl>
2025-01-05 19:18:57 +0100

    adding changes to navbar -adding a responsive architecture for editing data in the navbar -creating a view resembling the prototype -adding scss for easier style management

--- front-end/src/views/Landing.vue
+++ front-end/src/views/Landing.vue
@@ -1,9 +1,18 @@
-<script setup lang="ts"></script>
+<script lang="ts" setup>
+import Navbar from '@/components/Navbar.vue';
+const navLinks = [
+  { label: "Oferta", href: "/" },
+  { label: "Kontakt", href: "/"},
+  { label: "Panel", href: "/",active:true}
+];
+</script>
 
 <template>
-    <div>
-      start page
-    </div>
+  <Navbar :links="navLinks">
+    <template #logo>
+      <img src="@/assets/vue.svg" alt="App Logo" />
+    </template>
+  </Navbar>
 </template>
-  
-  
+
+
