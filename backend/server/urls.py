a213f05b490241785adacf4d97b52f5ed6e085d4
JakubPoblocki <jakubpoblocki1@gmail.com>
2025-01-10 16:44:45 +0100

    added swagger in urls

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -16,8 +16,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+'''
+Extending schema example
+@extend_schema(
+    summary="Custom summary for this endpoint",
+    description="Detailed description of what this endpoint does",
+    responses={200: "Successful response example"}
+)
+'''
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # OpenAPI schema generation endpoint
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Swagger UI
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    # Redoc UI
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
