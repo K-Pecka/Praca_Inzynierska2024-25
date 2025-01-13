4a9f8d6b607c5ae0b3bcccc2a5e2fbbe280448a8
JakubPoblocki <jakubpoblocki1@gmail.com>
2025-01-13 19:49:57 +0100

    xx

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,27 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': '/src',
+// vite.config.js
+const path = require('path')
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'lib/main.js'),
+      name: 'MyLib',
+      fileName: (format) => `my-lib.${format}.js`
     },
-  },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
 })
