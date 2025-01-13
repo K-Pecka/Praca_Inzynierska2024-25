a5b824eeebac5e2be4fa08900ae74808bd87b119
JakubPoblocki <jakubpoblocki1@gmail.com>
2025-01-13 19:55:29 +0100

    x

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 
-
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')  // Make sure '@' points to the 'src' folder
+    }
+  },
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'front-end/src/main.js'),
+      entry: path.resolve(__dirname, 'src/main.ts'),
       name: 'MyLib',
       fileName: (format) => `my-lib.${format}.js`
     },
