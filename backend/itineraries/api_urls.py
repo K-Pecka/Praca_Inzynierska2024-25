4cdfa2c9dcc74ba5e7ee7949a0b8074fc15201a7
s24893-pj <92208911+s24893-pj@users.noreply.github.com>
2025-01-25 21:21:01 +0100

    Update itineraries/api_urls

--- backend/itineraries/api_urls.py
+++ backend/itineraries/api_urls.py
@@ -6,14 +6,16 @@ from .api_views import (ItineraryActivityUpdateAPIView, ItineraryActivityListAPI
                         ItineraryActivityRetrieveAPIView)
 
 urlpatterns = [
-    path('itineraries/', ItineraryListAPIView.as_view(), name='itinerary-list'),
-    path('itineraries/create/', ItineraryCreateAPIView.as_view(), name='itinerary-create'),
-    path('itineraries/<int:pk>/', ItineraryRetrieveAPIView.as_view(), name='itinerary-detail'),
-    path('itineraries/<int:pk>/update/', ItineraryUpdateAPIView.as_view(), name='itinerary-update'),
-    path('itineraries/<int:pk>/delete/', ItineraryDestroyAPIView.as_view(), name='itinerary-delete'),
-    path('activities/', ItineraryActivityListAPIView.as_view(), name='activity-list'),
-    path('activities/create/', ItineraryActivityCreateAPIView.as_view(), name='activity-create'),
-    path('activities/<int:pk>/', ItineraryActivityRetrieveAPIView.as_view(), name='activity-detail'),
-    path('activities/<int:pk>/update/', ItineraryActivityUpdateAPIView.as_view(), name='activity-update'),
-    path('activities/<int:pk>/delete/', ItineraryActivityDestroyAPIView.as_view(), name='activity-delete'),
+    path('all/', ItineraryListAPIView.as_view(), name='itinerary-list'),
+    path('create/', ItineraryCreateAPIView.as_view(), name='itinerary-create'),
+    path('<int:pk>/', ItineraryRetrieveAPIView.as_view(), name='itinerary-detail'),
+    path('<int:pk>/update/', ItineraryUpdateAPIView.as_view(), name='itinerary-update'),
+    path('<int:pk>/delete/', ItineraryDestroyAPIView.as_view(), name='itinerary-delete'),
+    path('<int:itinerary_pk>/activities/all/', ItineraryActivityListAPIView.as_view(), name='activity-list'),
+    path('<int:itinerary_pk>/activities/create/', ItineraryActivityCreateAPIView.as_view(), name='activity-create'),
+    path('<int:itinerary_pk>/activities/<int:pk>/', ItineraryActivityRetrieveAPIView.as_view(), name='activity-detail'),
+    path('<int:itinerary_pk>/activities/<int:pk>/update/', ItineraryActivityUpdateAPIView.as_view(),
+         name='activity-update'),
+    path('<int:itinerary_pk>/activities/<int:pk>/delete/', ItineraryActivityDestroyAPIView.as_view(),
+         name='activity-delete'),
 ]
