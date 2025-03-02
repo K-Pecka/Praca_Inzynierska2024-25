e4e37f1ec9d6f0e252119ee069d813057f2121b0
KacperPecka <s25668@pjwstk.edu.pl>
2025-03-02 18:11:17 +0100

    Add budget look implementations

--- front-end/src/views/panel/children/ExpenseTracker.vue
+++ front-end/src/views/panel/children/ExpenseTracker.vue
@@ -4,82 +4,98 @@ import AppButton from "@/components/budget/AppButton.vue";
 import AppProgress from "@/components/budget/AppProgress.vue";
 import ExpenseList from "@/components/budget/ExpenseList.vue";
 import Section from "@/components/common/Section.vue";
+import { ref } from "vue";
 const budget = 1500;
 const spent = 322;
 const remaining = budget - spent;
 const expenses = [
-  { id: 1, title: "Tytuł", category: "jedzenie", date: "26.02.2025", amount: 322, notes: "Notatki bla bla" }
+  {
+    id: 1,
+    title: "Tytuł",
+    category: "jedzenie",
+    date: "26.02.2025",
+    amount: 322,
+    notes: "Notatki bla bla",
+  },
 ];
+const categories = ref(["Jedzenie", "Transport", "Nocleg", "Rozrywka"]);
+const participants = ref(["Jan", "Kasia", "Tomek"]);
 </script>
 
 <template>
   <Section>
     <template #title>
-      <h1>Wakacje we fancji</h1>
       <div class="d-flex justify-space-between align-center">
-        <h1 class="title">Budżet</h1>
-      <AppButton class="add-button">Dodaj wydatek</AppButton>
+        <h1 class="title">Wydatki</h1>
+        <AppButton class="add-button">Dodaj wydatek</AppButton>
       </div>
-      
     </template>
     <template #content>
       <div class="summary">
-      <AppCard class="summary-card">
-        <h3>Budżet</h3>
-        <p class="amount">{{ budget }} EUR</p>
+        <AppCard class="summary-card">
+          <h3>Budżet</h3>
+          <p class="amount">{{ budget }} EUR</p>
+        </AppCard>
+        <AppCard class="summary-card">
+          <h3>Wydano</h3>
+          <AppProgress :value="spent" :max="budget" />
+          <div class="d-flex justify-space-between" style="margin-top: 10%">
+            <p class="spent">{{ spent }} EUR</p>
+            <p class="percent">{{ ((spent / budget) * 100).toFixed(2) }}%</p>
+          </div>
+        </AppCard>
+        <AppCard class="summary-card">
+          <h3>Pozostało</h3>
+          <p class="remaining">{{ remaining }} EUR</p>
+        </AppCard>
+      </div>
+
+      <AppCard class="expenses">
+        <v-card class="pa-4 mt-4 expenses-card">
+          <div class="d-flex justify-space-between align-center">
+            <h3 class="mb-2">Wydatki</h3>
+            <div class="filters">
+              <v-select label="Kategoria" :items="categories" variant="outlined"  class="filter-item"></v-select>
+              <v-select label="Uczestnicy" :items="participants" variant="outlined" class="filter-item"></v-select>
+              <v-text-field label="Data od" type="date" variant="outlined" class="filter-item"></v-text-field>
+              <v-text-field label="Data do" type="date" variant="outlined" class="filter-item"></v-text-field>
+            </div>
+          </div>
+        </v-card>
+
+        <ExpenseList :expenses="expenses" />
       </AppCard>
-      <AppCard class="summary-card">
-        <h3>Wydano</h3>
-        <AppProgress :value="spent" :max="budget" />
-        <div class="d-flex justify-space-between" style="margin-top: 10%;">
-          <p class="spent">{{ spent }} EUR</p>
-          <p class="percent">{{ (spent / budget * 100).toFixed(2) }}%</p>
-        </div>
-        
-      </AppCard>
-      <AppCard class="summary-card">
-        <h3>Pozostało</h3>
-        <p class="remaining">{{ remaining }} EUR</p>
-      </AppCard>
-    </div>
-    
-    <AppCard class="expenses">
-      <h2 class="section-title">Wydatki</h2>
-      <ExpenseList :expenses="expenses" />
-    </AppCard>
-    
-    <div class="charts">
-      <AppCard class="chart-card"><h2 class="section-title">Wydatki - Kategorie</h2></AppCard>
-      <AppCard class="chart-card"><h2 class="section-title">Wydatki - Uczestnicy</h2></AppCard>
-    </div>
+
+      <div class="charts">
+        <AppCard class="chart-card"
+          ><h2 class="section-title">Wydatki - Kategorie</h2></AppCard
+        >
+        <AppCard class="chart-card"
+          ><h2 class="section-title">Wydatki - Uczestnicy</h2></AppCard
+        >
+      </div>
     </template>
   </Section>
 </template>
 
-<style scoped>
-h1{
+<style scoped lang="scss">
+h1 {
   font-size: 2rem;
   text-align: start;
 }
-h3{
+h3 {
+  display: flex;
   font-size: 1.5rem;
 }
-.amount, .remaining{
+.amount,
+.remaining {
   font-size: 2.2rem;
 }
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-}
-
 .title {
   font-size: 26px;
   font-weight: bold;
   color: #333;
 }
-
 .add-button {
   background-color: #4a90e2;
   color: white;
@@ -108,13 +124,11 @@ h3{
   font-weight: bold;
   color: #333;
 }
-
 .spent {
   font-size: 18px;
   color: #4a90e2;
   font-weight: bold;
 }
-
 .remaining {
   color: #2e7d32;
   font-weight: bold;
@@ -125,6 +139,15 @@ h3{
   padding: 20px;
   border-radius: 12px;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+
+.filters {
+  display: flex;
+  gap: 1rem;
+  justify-content: flex-end;
+}
+.filter-item {
+  width: 10vw;
 }
 
 .section-title {
@@ -147,5 +170,8 @@ h3{
   padding: 20px;
   border-radius: 12px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+.filter-item :deep(.v-input__control){
+  border-radius: 0.5rem;
 }
 </style>
