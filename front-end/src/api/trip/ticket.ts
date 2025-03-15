082697bfcc2c7836eaa02658a6e1eba46d258dbc
KacperPecka <s25668@pjwstk.edu.pl>
2025-03-15 13:16:29 +0100

    Create the basic infrastructure for adding a ticket

---
+++ front-end/src/api/trip/ticket.ts
@@ -0,0 +1,19 @@
+import { apiEndpoints, fetchData, setParam } from "../apiEndpoints";
+import { Ticket, NewTrip } from "@/type/interface";
+
+export const createPlan = async (
+  ticket: Ticket,
+  body: Record<string, string>={},
+  param: Record<string, string> = {}
+) => {
+  const { data, error } = await fetchData<Ticket>(
+    setParam(apiEndpoints.plan.create, param),
+    { body: JSON.stringify(ticket) },
+    "POST"
+  );
+  if (error) {
+    return;
+  }
+
+  return data;
+};
---
+++ front-end/src/stores/useTicketStore.ts
@@ -0,0 +1,11 @@
+import { defineStore } from "pinia";
+import { useUtilStore } from "./utils/useUtilStore";
+export const useTicketStore = defineStore("ticket", () => {
+    const {getTripId} = useUtilStore();
+   const createTicket = (file:File,data:Record<string, string>) =>{
+    console.log(file,data,getTripId('tripId'));
+    console.log(data);
+    console.log(getTripId('tripId'));
+   }
+    return {createTicket}
+});
