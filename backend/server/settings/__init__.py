6b7f4f1de32f5d59df46e7ccfaf13496b0d68036
JakubPoblocki <jakubpoblocki1@gmail.com>
2025-03-19 22:18:42 +0100

    Fix init for prod test

--- backend/server/settings/__init__.py
+++ backend/server/settings/__init__.py
@@ -4,7 +4,7 @@ ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
 
 if ENVIRONMENT == "production":
     from .production import *
-elif ENVIRONMENT == "testr":
+elif ENVIRONMENT == "test":
     from .test import *
 else:
     from .development import *
