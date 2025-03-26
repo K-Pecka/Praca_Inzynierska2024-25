9cf42f4219598007cf43b350abcd9bb06b5ab884
s25770-pj <s25770@pjwstk.edu.pl>
2025-03-26 16:59:13 +0100

    Undo changes for testing heroku buildup

--- backend/trips/urls.py
+++ backend/trips/urls.py
@@ -11,9 +11,9 @@ from trips.views.trip_views import TripCreateAPIView, TripRetrieveAPIView, TripL
 
 urlpatterns = [
     # Trip URLs
-    # path('create/', TripCreateAPIView.as_view(), name='trip-create'),
-    # path('<int:pk>/', TripRetrieveAPIView.as_view(), name='trip-retrieve'),
-    # path('all/', TripListAPIView.as_view(), name='trip-list'),
+    path('create/', TripCreateAPIView.as_view(), name='trip-create'),
+    path('<int:pk>/', TripRetrieveAPIView.as_view(), name='trip-retrieve'),
+    path('all/', TripListAPIView.as_view(), name='trip-list'),
     path('<int:pk>/update/', TripUpdateAPIView.as_view(), name='trip-update'),
     path('<int:pk>/delete/', TripDestroyAPIView.as_view(), name='trip-delete'),
 
