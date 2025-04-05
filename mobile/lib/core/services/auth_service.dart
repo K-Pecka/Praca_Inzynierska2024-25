028c7181aabb7ec1ceed7ea0e5ebb1c85b5bd489
s24893-pj <92208911+s24893-pj@users.noreply.github.com>
2025-04-05 08:07:56 +0200

    create /core/services/auth_service.dart

---
+++ mobile/lib/core/services/auth_service.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class AuthService {
+  final String _baseUrl = 'https://api.plannder.com';
+
+  Future<Map<String, dynamic>> login({
+    required String email,
+    required String password,
+  }) async {
+    final url = Uri.parse('$_baseUrl/user_auth/login/');
+    final headers = {
+      'accept': 'application/json',
+      'Content-Type': 'application/json',
+      'X-CSRFTOKEN': 'emQDUV5mbHvU0I7yKcVmQEyCwv5xrgqPZWnipscqu7sYR152wgYrPtvW1Jj1wJYK',
+    };
+
+    final body = jsonEncode({
+      'email': email,
+      'password': password,
+    });
+
+    final response = await http.post(url, headers: headers, body: body);
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Błąd: ${response.statusCode}');
+    }
+  }
+}
