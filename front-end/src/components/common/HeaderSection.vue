bef88f234a6dce711892a02e0cd60cbdb6122d5d
AndrzejjE <s25222@pjwstk.edu.pl>
2025-04-12 19:14:17 +0200

    Better mobile responsiveness
    - TicketsView.vue
    - HeaderSection.vue
    - Section.vue

--- front-end/src/components/common/HeaderSection.vue
+++ front-end/src/components/common/HeaderSection.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { useRoute } from "vue-router";
+import {useRoute} from "vue-router";
 import {useTripStore} from "@/stores";
 
 const route = useRoute();
@@ -8,41 +8,49 @@ const {getTripDetails} = useTripStore();
 const {data: tripData, isLoading, error} = getTripDetails(tripId);
 </script>
 <template>
-    <section class="section">
-      <header class="section__title">
-        <h1 class="trip-title" v-if="!isLoading && !error">{{ tripData?.name }}</h1>
-        <h1 class="trip-title" v-else>...</h1>
-      </header>
-      <div class="section__subtitle">
-        <slot name="subtitle"></slot>
-      </div>
-    </section>
-  </template>
-  <style scoped lang="scss">
- * {
-    width: 100%;
-    text-align: start;
-  }
-  .section__title{
-    margin: 0 0 3rem 0;
-  }
-  .section__subtitle{
-    margin: 3rem 0 3rem 0;
-  }
-  .trip-title {
+  <section class="section">
+    <header class="section__title">
+      <h1 class="trip-title" v-if="!isLoading && !error">{{ tripData?.name }}</h1>
+      <h1 class="trip-title" v-else>...</h1>
+    </header>
+    <div class="section__subtitle">
+      <slot name="subtitle"></slot>
+    </div>
+  </section>
+</template>
+<style scoped lang="scss">
+* {
+  width: 100%;
+  text-align: start;
+}
+
+.section__title {
+  margin: 0 0 2rem 0;
+}
+
+.section__subtitle {
+  margin: 2rem 0 2rem 0;
+}
+
+.trip-title {
+  font-family: Inter,serif;
   font-size: 2rem;
   font-weight: 700;
   text-align: start;
-  margin: 0 0 3rem 0;
+  margin: 0 0 2rem 0;
 }
+
 .button-container {
   display: flex;
   justify-content: flex-end;
 }
+
 .title-container {
-    width: 100%;
+  width: 100%;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
 }
-  </style>
+
+
+</style>
