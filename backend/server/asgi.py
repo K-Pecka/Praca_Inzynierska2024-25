96190ad55abd516ec3b4b8ca0165328632dd63b9
s25770-pj <s25770@pjwstk.edu.pl>
2025-04-25 18:17:17 +0200

    x

--- backend/server/asgi.py
+++ backend/server/asgi.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
+import django
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
@@ -17,7 +19,7 @@ import chats.routing
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
 django_asgi_app = get_asgi_application()
-
+django.setup()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
