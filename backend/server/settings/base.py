497d012c8d72e7668ff83ff7747e804dcaa2cb76
s25770-pj <s25770@pjwstk.edu.pl>
2025-04-28 21:14:51 +0200

    x

--- backend/server/settings/base.py
+++ backend/server/settings/base.py
@@ -115,7 +115,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION': True,
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
 }
