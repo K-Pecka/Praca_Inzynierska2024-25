c6721c63b5157dda9403f2df4fe34d5b3dc4d946
s25770-pj <s25770@pjwstk.edu.pl>
2025-04-29 12:14:12 +0200

    x

--- backend/server/views.py
+++ backend/server/views.py
@@ -3,7 +3,7 @@ from django.http import FileResponse, Http404
 from django.conf import settings
 
 def download_ticket(request, filename):
-    file_path = os.path.join(settings.MEDIA_ROOT, filename)
+    file_path = os.path.join(settings.MEDIA_ROOT, 'tickets', filename)
 
     if os.path.exists(file_path):
         return FileResponse(open(file_path, 'rb'), as_attachment=True, filename=filename)
