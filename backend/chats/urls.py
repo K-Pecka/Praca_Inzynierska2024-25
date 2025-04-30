1a7ccae9a3cfa794e7c12b24983ddea58aa9445e
s24893-pj <92208911+s24893-pj@users.noreply.github.com>
2025-05-01 00:17:28 +0200

    fix urls

--- backend/chats/urls.py
+++ backend/chats/urls.py
@@ -6,17 +6,19 @@ from chats.views.chatroom_views import ChatroomCreateAPIView, ChatroomRetrieveAP
     ChatroomUpdateAPIView, ChatroomDestroyAPIView
 
 urlpatterns = [
-    # Chatroom URLs
-    path('create/', ChatroomCreateAPIView.as_view(), name='chatroom-create'),
-    path('<int:pk>/', ChatroomRetrieveAPIView.as_view(), name='chatroom-retrieve'),
-    path('all/', ChatroomListAPIView.as_view(), name='chatroom-list'),
-    path('update/', ChatroomUpdateAPIView.as_view(), name='chatroom-update'),
-    path('delete/', ChatroomDestroyAPIView.as_view(), name='chatroom-delete'),
+    path('chatroom/create/', ChatroomCreateAPIView.as_view(), name='chatroom-create'),
+    path('chatroom/<int:pk>/', ChatroomRetrieveAPIView.as_view(), name='chatroom-retrieve'),
+    path('chatroom/all/', ChatroomListAPIView.as_view(), name='chatroom-list'),
+    path('chatroom/<int:pk>/update/', ChatroomUpdateAPIView.as_view(), name='chatroom-update'),
+    path('chatroom/<int:pk>/delete/', ChatroomDestroyAPIView.as_view(), name='chatroom-delete'),
 
     # Message URLs
-    path('<int:room_pk>/chat-message/create/', MessageCreateAPIView.as_view(), name='chat-message-create'),
-    path('<int:room_pk>/chat-message/<int:pk>/', MessageRetrieveAPIView.as_view(), name='chat-message-retrieve'),
-    path('<int:room_pk>/chat-message/all/', MessageListAPIView.as_view(), name='chat-message-list'),
-    path('<int:room_pk>/chat-message/<int:pk>/update/', MessageUpdateAPIView.as_view(), name='chat-message-update'),
-    path('<int:room_pk>/chat-message/<int:pk>/delete/', MessageDestroyAPIView.as_view(), name='chat-message-delete'),
+    path('chatroom/<int:room_pk>/chat-message/create/', MessageCreateAPIView.as_view(), name='chat-message-create'),
+    path('chatroom/<int:room_pk>/chat-message/<int:pk>/', MessageRetrieveAPIView.as_view(),
+         name='chat-message-retrieve'),
+    path('chatroom/<int:room_pk>/chat-message/all/', MessageListAPIView.as_view(), name='chat-message-list'),
+    path('chatroom/<int:room_pk>/chat-message/<int:pk>/update/', MessageUpdateAPIView.as_view(),
+         name='chat-message-update'),
+    path('chatroom/<int:room_pk>/chat-message/<int:pk>/delete/', MessageDestroyAPIView.as_view(),
+         name='chat-message-delete'),
 ]
