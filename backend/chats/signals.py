554301387c59b42ccf3e8bfb8b88a5410ad68aa2
s24893-pj <92208911+s24893-pj@users.noreply.github.com>
2025-05-01 02:08:17 +0200

    x

--- backend/chats/signals.py
+++ backend/chats/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, m2m_changed
 from django.dispatch import receiver
 from chats.models import Chatroom
 from trips.models import Trip
@@ -8,9 +8,20 @@ from chats.choices import ChatroomType
 @receiver(post_save, sender=Trip)
 def create_announcement_chatroom(sender, instance, created, **kwargs):
     if created:
-        Chatroom.objects.create(
+        chatroom = Chatroom.objects.create(
             name=f"Ogłoszenia – {instance.name}",
-            type=ChatroomType.GROUP,  # albo 'announcement' jeśli masz osobny typ
+            type=ChatroomType.GROUP,
             trip=instance,
             creator=instance.creator,
         )
+        chatroom.members.set(instance.members.all())
+
+
+@receiver(m2m_changed, sender=Trip.members.through)
+def sync_chatroom_members_on_trip_members_update(sender, instance, action, pk_set, **kwargs):
+    if action == 'post_add':
+        try:
+            chatroom = Chatroom.objects.get(trip=instance, type=ChatroomType.GROUP)
+        except Chatroom.DoesNotExist:
+            return
+        chatroom.members.add(*pk_set)
