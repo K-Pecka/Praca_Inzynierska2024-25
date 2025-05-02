0d084da0508f93abe4018aa8dfc9713f9252d58e
s24893-pj <92208911+s24893-pj@users.noreply.github.com>
2025-05-02 22:18:05 +0200

    x

--- backend/chats/middleware/jwt_auth.py
+++ backend/chats/middleware/jwt_auth.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import AnonymousUser
 from users.models import CustomUser
 from channels.db import database_sync_to_async
 
-
 @database_sync_to_async
 def get_user(token):
     if not token:
@@ -17,29 +16,17 @@ def get_user(token):
     except Exception:
         return AnonymousUser()
 
-
 class JwtAuthMiddleware:
-    def __init__(self, inner):
-        self.inner = inner
+    def __init__(self, app):
+        self.app = app
 
-    def __call__(self, scope):
-        return JwtAuthMiddlewareInstance(scope, self.inner)
-
-
-class JwtAuthMiddlewareInstance:
-    def __init__(self, scope, inner):
-        self.scope = scope
-        self.inner = inner
-
-    async def __call__(self, receive, send):
-        query_string = self.scope["query_string"].decode()
+    async def __call__(self, scope, receive, send):
+        query_string = scope.get("query_string", b"").decode()
         query_params = parse_qs(query_string)
         token = query_params.get("token", [None])[0]
 
-        self.scope["user"] = await get_user(token)
-        inner = self.inner(self.scope)
-        return await inner(receive, send)
-
+        scope["user"] = await get_user(token)
+        return await self.app(scope, receive, send)
 
 def JwtAuthMiddlewareStack(inner):
     return JwtAuthMiddleware(inner)
