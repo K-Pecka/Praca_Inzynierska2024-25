d7e459b536bb72b0437b4afaece343478b05867f
s24893-pj <92208911+s24893-pj@users.noreply.github.com>
2025-05-02 21:11:17 +0200

    x

--- backend/chats/consumers.py
+++ backend/chats/consumers.py
@@ -1,13 +1,12 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-from .models import Message
-from users.models import UserProfile
+from .models import Message, Chatroom
 from asgiref.sync import sync_to_async
-
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_group_name = "chat_room"
+        self.room_name = self.scope["url_route"]["kwargs"]["room_id"]
+        self.room_group_name = f"chat_{self.room_name}"
 
         await self.channel_layer.group_add(
             self.room_group_name,
@@ -23,28 +22,42 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data):
         data = json.loads(text_data)
-        message = data["message"]
-        username = data["username"]
+        message = data["content"]
+        user = self.scope["user"]
 
-        user_profile = await sync_to_async(UserProfile.objects.get)(user__username=username)
+        if user.is_anonymous:
+            await self.close()
+            return
 
-        await self.save_message(user_profile, message)
+        # Zapisz wiadomość
+        await self.save_message(user.profile, int(self.room_name), message)
 
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 "type": "chat_message",
                 "message": message,
-                "username": username,
+                "sender_id": user.profile.id,
+                "created": self.get_now(),
             }
         )
 
     async def chat_message(self, event):
         await self.send(text_data=json.dumps({
-            "message": event["message"],
-            "username": event["username"],
+            "content": event["message"],
+            "sender_id": event["sender_id"],
+            "created": event["created"],
         }))
 
     @sync_to_async
-    def save_message(self, user_profile, message):
-        Message.objects.create(profile=user_profile, content=message)
+    def save_message(self, profile, room_id, content):
+        room = Chatroom.objects.get(id=room_id)
+        return Message.objects.create(
+            profile=profile,
+            chatroom=room,
+            content=content
+        )
+
+    def get_now(self):
+        from django.utils.timezone import now
+        return now().isoformat()
