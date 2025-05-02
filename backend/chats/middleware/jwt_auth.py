bc430e79d857ae9670a344fcbeab4fb963953516
s24893-pj <92208911+s24893-pj@users.noreply.github.com>
2025-05-02 21:49:15 +0200

    x

--- backend/chats/middleware/jwt_auth.py
+++ backend/chats/middleware/jwt_auth.py
@@ -1,9 +1,21 @@
 import jwt
+from urllib.parse import parse_qs
 from django.conf import settings
 from django.contrib.auth.models import AnonymousUser
 from users.models import CustomUser
 from channels.db import database_sync_to_async
-from urllib.parse import parse_qs
+
+
+@database_sync_to_async
+def get_user(token):
+    if not token:
+        return AnonymousUser()
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+        user_id = payload.get("user_id")
+        return CustomUser.objects.get(id=user_id)
+    except Exception:
+        return AnonymousUser()
 
 
 class JwtAuthMiddleware:
@@ -11,33 +23,22 @@ class JwtAuthMiddleware:
         self.inner = inner
 
     def __call__(self, scope):
-        return JwtAuthMiddlewareInstance(scope, self)
+        return JwtAuthMiddlewareInstance(scope, self.inner)
 
 
 class JwtAuthMiddlewareInstance:
-    def __init__(self, scope, middleware):
-        self.scope = dict(scope)
-        self.middleware = middleware
+    def __init__(self, scope, inner):
+        self.scope = scope
+        self.inner = inner
 
     async def __call__(self, receive, send):
         query_string = self.scope["query_string"].decode()
         query_params = parse_qs(query_string)
         token = query_params.get("token", [None])[0]
 
-        self.scope["user"] = await self.get_user(token)
-        inner = self.middleware.inner(self.scope)
+        self.scope["user"] = await get_user(token)
+        inner = self.inner(self.scope)
         return await inner(receive, send)
-
-    @database_sync_to_async
-    def get_user(self, token):
-        if not token:
-            return AnonymousUser()
-        try:
-            payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
-            user_id = payload.get("user_id")
-            return CustomUser.objects.get(id=user_id)
-        except Exception:
-            return AnonymousUser()
 
 
 def JwtAuthMiddlewareStack(inner):
