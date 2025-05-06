5b509e475b0b4ba70238c5d95b4d0613213c0532
s24893-pj <92208911+s24893-pj@users.noreply.github.com>
2025-05-06 21:43:43 +0200

    ,

--- mobile/lib/core/theme/themes.dart
+++ mobile/lib/core/theme/themes.dart
@@ -9,13 +9,3 @@ class AppColors {
   static const Color screenBackground = Colors.white;
   static const Color logout = Colors.red;
 }
-
-class AppColorsDark {
-  static const Color navigationBackground = Color(0xFF1A1A1A);
-  static const Color cardsBackground = Color(0xFF2B2B2B);
-  static const Color primary = Color(0xFF7C6AFF);
-  static const Color titleText = Colors.white;
-  static const Color subtitleText = Color(0xFFB0B0B0);
-  static const Color screenBackground = Color(0xFF121212);
-  static const Color logout = Color(0xFFFF6B6B);
-}
