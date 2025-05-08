96ea1cd61c92a59f9901c9d45dc0e685193c55e5
s25770-pj <s25770@pjwstk.edu.pl>
2025-05-08 23:20:10 +0200

    x

--- backend/dicts/validators.py
+++ backend/dicts/validators.py
@@ -6,5 +6,5 @@ from django.core.validators import RegexValidator
 phone_number_re = re.compile(r'^\+?(\d{1,3})?[-.\s]?(\(?\d{1,4}\)?)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$')
 validate_phone_number = RegexValidator(phone_number_re, 'Enter a valid phone number consisting only numbers')
 
-only_alphabetic_re = re.compile(r'^[a-zA-Z]+$')
-validate_only_alphabetic = RegexValidator(only_alphabetic_re, 'Enter a valid alphabetic code')
+only_alphabetic_re = re.compile(r'^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ]+$')
+validate_only_alphabetic = RegexValidator(only_alphabetic_re, 'Wprowadź poprawny alfabetystyczny ciąg znaków (bez cyfr i symboli).')
