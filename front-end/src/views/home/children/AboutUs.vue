0ebd2fbb21d621dc1a66053fe04db08f5356230c
s25770-pj <s25770@pjwstk.edu.pl>
2025-05-10 02:09:59 +0200

    x

--- front-end/src/views/home/children/AboutUs.vue
+++ front-end/src/views/home/children/AboutUs.vue
@@ -14,23 +14,21 @@ import {HeaderSection} from "@/components";
       />
     </template>
     <template #content>
-      <v-container>
-        <v-row justify="center" class="mb-10">
-          <v-col cols="12" sm="12" offset-md="0" md="10">
-            <div class="text-body-1 w-100">
-              <div
-                v-for="(paragraph, index) in aboutTeam.paragraphs"
-                :key="index"
-              >
-                <p class="text-justify text-h6">{{ paragraph.paragraph }}</p>
-                <template v-if="index == 0">
-                  <TeamMemberCard :members="aboutTeam.teamMembers" />
-                </template>
-              </div>
+      <v-row justify="center" class="mb-10">
+        <v-col cols="12" sm="12" offset-md="0" md="10">
+          <div class="text-body-1 w-100">
+            <div
+              v-for="(paragraph, index) in aboutTeam.paragraphs"
+              :key="index"
+            >
+              <p class="text-justify text-h6">{{ paragraph.paragraph }}</p>
+              <template v-if="index == 0">
+                <TeamMemberCard :members="aboutTeam.teamMembers" />
+              </template>
             </div>
-          </v-col>
-        </v-row>
-      </v-container>
+          </div>
+        </v-col>
+      </v-row>
     </template>
   </Section>
 </template>
