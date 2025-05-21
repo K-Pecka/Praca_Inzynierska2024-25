983f86305935f8049c130cb352927f8209463a95
KacperPecka <s25668@pjwstk.edu.pl>
2025-05-21 23:45:54 +0200

    Fix 'contacts' display in mobile version

--- front-end/src/views/home/children/AboutUs.vue
+++ front-end/src/views/home/children/AboutUs.vue
@@ -19,7 +19,7 @@ import {HeaderSection} from "@/components";
       <v-row justify="center" class="mb-10">
         <v-col cols="12" sm="12" offset-md="0" md="10">
           <div>
-            <span class="text-h5">
+            <span class="text-h5 text-justify">
               Jesteśmy grupą studentów, których połączyła pasja
            do podróży i chęć ułatwienia życia sobie i innym. Z własnego
             doświadczenia wiemy, jak trudno czasem zorganizować wspólny
