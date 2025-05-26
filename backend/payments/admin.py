d27e8fd94cf01debb4c4cd2e1a007b107140be06
s25770-pj <s25770@pjwstk.edu.pl>
2025-05-26 20:59:38 +0200

    Update frontend UI

--- backend/payments/admin.py
+++ backend/payments/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from payments.models import Order
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'is_paid', 'subscription_type', 'created_at')
+    list_filter = ('is_paid', 'subscription_type', 'created_at')
+    search_fields = ('user__email', 'stripe_session_id', 'subscription_type')
+    ordering = ('-created_at',)
