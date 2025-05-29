7890a520362229420d5c0c47e949a6c0f15df6bc
s25770-pj <s25770@pjwstk.edu.pl>
2025-05-29 02:40:24 +0200

    Update frontend UI

--- backend/payments/views.py
+++ backend/payments/views.py
@@ -103,7 +103,13 @@ class StripeWebhookView(APIView):
             print("== RAW INVOICE ==")
             print(f'invoiceeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee: {event}')
 
-            subscription_id = invoice["lines"]["data"]["parent"]["subscription_item_details"]["subscription"]
+            subscription_id = (
+                invoice.get("lines", {})
+                .get("data", [{}])[0]
+                .get("parent", {})
+                .get("subscription_item_details", {})
+                .get("subscription")
+            )
             print("Extracted subscription_id:", subscription_id)
 
             if not subscription_id:
